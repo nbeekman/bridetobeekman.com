37d59d6f6f3c1ee2e5f31646d7ebe438ffe96f1e
nbeekman <nathan.beekman1@gmail.com>
2016-11-29 22:36:46 -0700

    added google analytics and add content for the welcome section

--- src/js/components/welcome/styles/welcome.js
+++ src/js/components/welcome/styles/welcome.js
@@ -1,9 +1,33 @@
 const styles = {
   container: {
     width: '100%',
-    marginTop: '10px',
+    margin: '10px 0px 100px',
     padding: '0px 30px',
     boxSizing: 'border-box',
+  },
+  imgCont: {
+    display: 'inline-block',
+    width: '50%',
+    textAlign: 'right',
+  },
+  image: {
+    display: 'inline-block',
+    maxWidth: '600px',
+    height: '300px',
+    marginRight: '50px',
+  },
+  textCont: {
+    display: 'inline-block',
+    width: '50%',
+    position: 'relative',
+    top: '-90px',
+  },
+  text: {
+    margin: '0px',
+    maxWidth: '650px',
+  },
+  textBottomMargin: {
+    margin: '0px 0px 10px',
   },
 };
 
