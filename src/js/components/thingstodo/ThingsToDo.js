a46461528b1aa817e9ec95e279f00061a695cb07
nbeekman <nathan.beekman1@gmail.com>
2016-12-06 23:04:44 -0700

    placeholder text/styling for things to do section

--- src/js/components/thingstodo/ThingsToDo.js
+++ src/js/components/thingstodo/ThingsToDo.js
@@ -6,7 +6,33 @@ const ThingsToDo = () => (
     <h2 className="amatic sectionHeader">
       <span>&#171;</span> Things To Do <span>&#187;</span>
     </h2>
-    <p style={ { height: '1000px' } }>content</p>
+    <h3 style={ styles.description }>Here are some suggestions we think you might enjoy while in Denver!</h3>
+    <div style={ styles.headingListCont }>
+      <h4 style={ styles.heading }>To Do</h4>
+      <ul style={ styles.list }>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
+      <ul style={ styles.listLast }>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
+    </div>
+    <div style={ styles.headingListCont }>
+      <h4 style={ styles.heading }>To Eat</h4>
+      <ul style={ styles.list }>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
+      <ul style={ styles.listLast }>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
+    </div>
   </div>
 );
 
--- src/js/components/thingstodo/styles/thingsToDo.js
+++ src/js/components/thingstodo/styles/thingsToDo.js
@@ -5,6 +5,29 @@ const styles = {
     padding: '0px 30px',
     boxSizing: 'border-box',
   },
+  description: {
+    textAlign: 'center',
+    fontWeight: 'normal',
+    marginTop: '0px',
+  },
+  heading: {
+    margin: '0px',
+  },
+  headingListCont: {
+    textAlign: 'center',
+    marginBottom: '20px',
+  },
+  list: {
+    display: 'inline-block',
+    listStyle: 'none',
+    padding: '0px',
+    marginRight: '100px',
+  },
+  listLast: {
+    display: 'inline-block',
+    listStyle: 'none',
+    padding: '0px',
+  },
 };
 
 export default styles;
