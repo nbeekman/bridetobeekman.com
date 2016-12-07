836f0e82f749dac6ebe2ea806ca163c302ed92d1
nbeekman <nathan.beekman1@gmail.com>
2016-12-06 22:28:30 -0700

    added party section content

--- src/js/components/party/Party.js
+++ src/js/components/party/Party.js
@@ -6,7 +6,18 @@ const Party = () => (
     <h2 className="amatic sectionHeader">
       <span>&#171;</span> Party <span>&#187;</span>
     </h2>
-    <p style={ { height: '1000px' } }>content</p>
+    <div style={ styles.partyContentLeft }>
+      <h3 style={ styles.partyHeading }>Ceremony</h3>
+      <p style={ styles.time }>4:30 PM</p>
+      <a href="http://www.botanicgardens.org/" target="_blank" style={ styles.botanicGardens } />
+      <p>1007 York St, Denver, CO 80206</p>
+    </div>
+    <div style={ styles.partyContentRight }>
+      <h3 style={ styles.partyHeading }>Reception</h3>
+      <p style={ styles.time }>6:00 PM</p>
+      <a href="http://www.mossdenver.com/" target="_blank" style={ styles.mossDenver } />
+      <p>200 Santa Fe Dr, Denver, CO 80223</p>
+    </div>
   </div>
 );
 
--- src/js/components/party/styles/party.js
+++ src/js/components/party/styles/party.js
@@ -1,9 +1,51 @@
+import MossDenver from 'assets/images/logos/moss-denver.svg';
+import BotanicGardens from 'assets/images/logos/denver-botanic-gardens.svg';
+
 const styles = {
   container: {
     width: '100%',
     margin: '10px 0px 100px',
     padding: '0px 30px',
     boxSizing: 'border-box',
+  },
+  partyContentLeft: {
+    display: 'inline-block',
+    width: '50%',
+    boxSizing: 'border-box',
+    textAlign: 'center',
+    verticalAlign: 'top',
+    borderRight: '1px solid #000000',
+    padding: '20px 0px',
+  },
+  partyContentRight: {
+    display: 'inline-block',
+    width: '50%',
+    boxSizing: 'border-box',
+    textAlign: 'center',
+    verticalAlign: 'top',
+    padding: '20px 0px',
+  },
+  partyHeading: {
+    marginBottom: '0px',
+  },
+  time: {
+    marginTop: '0px',
+  },
+  mossDenver: {
+    background: `url(${MossDenver}) no-repeat center center`,
+    backgroundSize: '100% 100%',
+    height: '70px',
+    width: '350px',
+    display: 'block',
+    margin: '10px auto',
+  },
+  botanicGardens: {
+    background: `url(${BotanicGardens}) no-repeat center center`,
+    backgroundSize: '100% 50px',
+    height: '70px',
+    width: '350px',
+    display: 'block',
+    margin: '10px auto',
   },
 };
 
