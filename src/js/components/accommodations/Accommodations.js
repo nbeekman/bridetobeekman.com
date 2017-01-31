c536fe63bacf469d8c722890179dfb31c2fd7deb
nbeekman <nathan.beekman1@gmail.com>
2017-01-30 19:13:18 -0700

    adding accommodations!

--- src/js/components/accommodations/Accommodations.js
+++ src/js/components/accommodations/Accommodations.js
@@ -6,9 +6,57 @@ const Accommodations = () => (
     <h2 className="amatic sectionHeader">
       <span>&#171;</span> Accommodations <span>&#187;</span>
     </h2>
-    <div style={ { textAlign: 'center', height: '150px' } }>
-      <p>We&#8217;re still scouting out some great spots for everyone to stay at.</p>
-      <p>Please stay tuned and we&#8217;ll have more information soon!</p>
+    <div style={ styles.content }>
+      <h3 style={ styles.description }>Mention the Minich-Beekman wedding to receive a discounted rate at any of these hotels!</h3>
+      <ul style={ styles.list }>
+        <li style={ styles.listItem }>
+          <h4 style={ styles.hotelName }>Grand Hyatt</h4>
+          <p style={ styles.text }>1750 Welton Street, Denver, CO 80202</p>
+          <p style={ styles.text }>
+            <a
+              href="https://denver.grand.hyatt.com/en/hotel/home.html"
+              target="_blank"
+              style={ styles.underline }
+            >
+              Website
+            </a>
+          </p>
+          <p style={ styles.text }>Phone: <a href="tel:303-295-1234">303.295.1234</a></p>
+        </li>
+        <li style={ styles.listItem }>
+          <h4 style={ styles.hotelName }>Hilton Garden Inn</h4>
+          <p style={ styles.text }>1400 Welton Street, Denver, CO, 80202</p>
+          <p style={ styles.text }>
+            <a
+              href="http://hiltongardeninn3.hilton.com/en/hotels/colorado/hilton-garden-inn-denver-downtown-DENDDGI/index.html"
+              target="_blank"
+              style={ styles.underline }
+            >
+              Website
+            </a>
+          </p>
+          <p style={ styles.text }>Phone: <a href="tel:303-603-8000">303.603.8000</a></p>
+        </li>
+        <li style={ styles.listItemLast }>
+          <h4 style={ styles.hotelName }>Hyatt Regency</h4>
+          <p style={ styles.text }>650 15th Street, Denver, CO 80202</p>
+          <p style={ styles.text }>
+            <a
+              href="https://denver.regency.hyatt.com/en/hotel/home.html?icamp=denverregencyredirect"
+              target="_blank"
+              style={ styles.underline }
+            >
+              Website
+            </a>
+          </p>
+          <p style={ styles.text }>Phone: <a href="tel:303-436-1234">303.436.1234</a></p>
+          <p style={ styles.text }>
+            <a href="https://aws.passkey.com/go/minichbeekman2017" style={ styles.underline }>
+              Discounted Online Reservation Link
+            </a>
+          </p>
+        </li>
+      </ul>
     </div>
   </div>
 );
--- src/js/components/accommodations/styles/accommodations.js
+++ src/js/components/accommodations/styles/accommodations.js
@@ -4,6 +4,40 @@ const styles = {
     padding: '0px 30px',
     boxSizing: 'border-box',
   },
+  content: {
+    textAlign: 'center',
+  },
+  description: {
+    textAlign: 'center',
+    fontWeight: 'normal',
+    marginTop: '0px',
+  },
+  list: {
+    listStyle: 'none',
+    padding: '0px',
+  },
+  listItem: {
+    display: 'inline-block',
+    textAlign: 'left',
+    verticalAlign: 'top',
+    width: '300px',
+    marginRight: '20px',
+  },
+  listItemLast: {
+    display: 'inline-block',
+    textAlign: 'left',
+    verticalAlign: 'top',
+    width: '300px',
+  },
+  hotelName: {
+    textAlign: 'center',
+  },
+  text: {
+    margin: '0px 0px 5px',
+  },
+  underline: {
+    textDecoration: 'underline',
+  },
 };
 
 export default styles;
